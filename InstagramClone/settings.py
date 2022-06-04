44c1bf5f45628bbaa230ce7d2fada6d47df08566
EmmaK <emmaculatewkamau@gmail.com>
2022-06-04 09:10:30 +0300

    Add login view, define a decorator for unauthenticated user

--- InstagramClone/settings.py
+++ InstagramClone/settings.py
@@ -79,20 +79,19 @@ WSGI_APPLICATION = 'InstagramClone.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DATABASE_NAME'),
-        'USER': os.environ.get('DATABASE_USER'),
-        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
-        'HOST': 'localhost',
-        'PORT': ''
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': os.environ.get('DATABASE_NAME'),
+    #     'USER': os.environ.get('DATABASE_USER'),
+    #     'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+    #     'HOST': '',
+    #     'PORT': ''
+    # }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
--- app/views.py
+++ app/views.py
@@ -1,6 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .decorators import *
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def homepage(request):
+
     context={}
     return render(request,'index.html',context=context)
+
+@unauthenticated_user
+def loginuser(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, "Username or Password is incorrect")
+            
+    context = {}
+    return render (request, 'accounts/login.html', context=context)
