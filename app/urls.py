7409c5d5bf52c23b7429660155b10ffc7856ca28
EmmaK <emmaculatewkamau@gmail.com>
2022-06-04 12:50:54 +0300

    Reset password design and logic

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns=[
     path('',views.homepage,name='homepage'),
     path('login/',views.loginuser,name='login'),
-    path('signup/',views.signup,name='signup')
+    path('signup/',views.signup,name='signup'),
+    path('logout/',views.logoutuser,name='logout'),
+
+    path('reset_password/',auth_views.PasswordResetView.as_view(template_name="accounts/password_reset.html"),name="reset_password"),
+    path('reset_password_sent/',auth_views.PasswordResetDoneView.as_view(template_name="accounts/password_reset_sent.html"),name="password_reset_done"),
+    path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name="accounts/password_reset_form.html"),name="password_reset_confirm"),
+    path('reset_password_complete/',auth_views.PasswordResetCompleteView.as_view(template_name="accounts/password_reset_done.html"),name="password_reset_complete"),
+
 ]
--- app/views.py
+++ app/views.py
@@ -43,3 +43,8 @@ def signup(request):
         'form': form,
     }
     return render (request, 'accounts/signup.html', context=context)
+
+def logoutuser(request):
+
+    logout(request)
+    return render (request, 'accounts/login.html')
