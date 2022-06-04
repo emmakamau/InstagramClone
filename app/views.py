abd555b6904eb4f6a84b862c9ba02b3f0055499c
EmmaK <emmaculatewkamau@gmail.com>
2022-06-04 16:00:48 +0300

    Add and style navbar

--- app/views.py
+++ app/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .decorators import *
 from .forms import *
 from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
+# @login_required(login_url='login')
 def homepage(request):
 
     context={}
