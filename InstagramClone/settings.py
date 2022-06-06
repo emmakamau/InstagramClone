f6d6dd1a5952ac50ced344c800cb9a41046cc0a2
EmmaK <emmaculatewkamau@gmail.com>
2022-06-06 15:00:32 +0300

    Add allowed hosts

--- InstagramClone/settings.py
+++ InstagramClone/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['inst-clone-emz.herokuapp.com','127.0.0.1']
 
 
 # Application definition
